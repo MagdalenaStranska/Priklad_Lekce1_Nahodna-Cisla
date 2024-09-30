ed9aec8e096fc14917b68c26148745d03af802e1
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-09-30 23:17:05 +0200

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,3 @@
+// Založte si JavaScriptový program a pomocí document.body.innerHTML a funkce Math.random zobrazte na stránce náhodné číslo. Zkuste stránku několikrát po sobě obnovit a ověřte si, že pokaždé obdržíte jiné číslo.
+
+document.body.innerHTML += Math.random() * 100
